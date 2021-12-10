f8272f13b9d255247bdd35d3566c374cfe753975
Matthew Landen <mlanden1221@gmail.com>
2021-12-10 08:06:03 -0800

    ad message when user joins room

--- app.js
+++ app.js
@@ -22,17 +22,20 @@ app.get('/:id', (req, res) => {
 io.on('connection', (socket) => {
     console.log(`User '${socket.id}' connected`);
 
+    // connect new user to room and send back username to notify room
     socket.on('join_room', (data) => {
         console.log(`Room data = ${data}`);
         socket.join(data.room);
         socket.to(data.room).emit('joined_room', data.user);
     });
 
+    // Send message to room
     socket.on('send_message', (data) => {
         console.log(`Message data = ${data}`);
         socket.to(data.room).emit('receive_message', data);
     });
 
+    // Disconnect user from room
     socket.on('disconnect', () => {
         console.log(`${socket.id} disconnected`);
     });
