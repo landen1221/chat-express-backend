7efa15948e0f5903d1d9cc3783ab171c15bd619d
Matthew Landen <mlanden1221@gmail.com>
2021-12-10 10:33:00 -0800

    clean up code-base for production

--- app.js
+++ app.js
@@ -20,18 +20,15 @@ app.get('/:id', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log(`User '${socket.id}' connected`);
-
-    // connect new user to room and send back username to notify room
+    // connect new user to room
+    // send back username to notify room
     socket.on('join_room', (data) => {
-        console.log(`Room data = ${data}`);
         socket.join(data.room);
         socket.to(data.room).emit('joined_room', data.user);
     });
 
     // Send message to room
     socket.on('send_message', (data) => {
-        console.log(`Message data = ${data}`);
         socket.to(data.room).emit('receive_message', data);
     });
 
