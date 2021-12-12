ddf4471f8920593b6c9a098b1c46131f9475b0f0
Matthew Landen <mlanden1221@gmail.com>
2021-12-12 12:57:27 -0800

    add temporary data storage

--- app.js
+++ app.js
@@ -19,16 +19,22 @@ app.get('/:id', (req, res) => {
     res.send('Hello world');
 });
 
+const msgObj = {};
+
 io.on('connection', (socket) => {
     // connect new user to room
     // send back username to notify room
     socket.on('join_room', (data) => {
         socket.join(data.room);
         socket.to(data.room).emit('joined_room', data.user);
+        socket.emit('get_messages', msgObj[data.room]);
     });
 
     // Send message to room
     socket.on('send_message', (data) => {
+        if (!msgObj[data.room]) msgObj[data.room] = [data];
+        else msgObj[data.room].push(data);
+
         socket.to(data.room).emit('receive_message', data);
     });
 
